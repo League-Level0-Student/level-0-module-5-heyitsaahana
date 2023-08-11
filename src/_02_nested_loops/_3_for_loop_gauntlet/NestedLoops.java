ec3d2d9d9cac08d66fe8023be85c48eca1594cb1
league <league@imac-34.attlocal.net>
2023-08-11 16:57:49 -0700

    nested loops

--- src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
@@ -11,29 +11,41 @@ public class NestedLoops {
 		//for(int x=100;x>-1;x--) {
 		//System.out.println(x);
 		//}
-	//	for(int y=2;y<101;y=y+2) {
+		//	for(int y=2;y<101;y=y+2) {
 		//	System.out.println( y  );
-//}
+		//}
 		//for(int t=1;t<100;t=t+2) {
 		//	System.out.println(t);
 		//}
 		//for(int s=1;s<501;s++) {
-			//if(s%2==0) {
-			//System.out.println(s+ " is even");
+		//if(s%2==0) {
+		//System.out.println(s+ " is even");
 		//	}else {
-			//	System.out.println(s+ " is odd");
-		
+		//	System.out.println(s+ " is odd");
+
 		//for(int b=0;b<778;b=b+7) {
-			//System.out.println(b);
-	//	}
-		for(int j=2010, u=0;j<2024;j++,u++) {
-		System.out.println("In "+j+" I was "+ u+" years old" );
-		}
-		
+		//System.out.println(b);
+		//	}
+		//for(int j=2010, u=0;j<2024;j++,u++) {
+		//	System.out.println("In "+j+" I was "+ u+" years old" );
+		//}
+		//for(int m=0;m<3;m++) {
+		//for(int p=0;p<3;p++) {
+		//	System.out.println(m+" "+p);
+
+		//}
+		//	}		
+		for(int k=0 ;k<3; k++) {
+		for(int c=1 ;c<4 ;c++) {
+			System.out.print(c+k*3);
 			}
-			
-			
-//}
+		System.out.println();
 		}
+
+	}
+
+
 	//}
+}
+//}
 
